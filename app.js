79b4c42997a5266244be16385e651c19ba3e5067
D-Haran <57646458+D-Haran@users.noreply.github.com>
2021-01-18 08:35:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var spotify = true;
 
 window.onload = function() {
   var inputField = document.getElementById("gsc-i-id1");
-  inputField.setAttribute("placeholder", "What Lyrics do you Have in Mind?");
-  inputField.setAttribute("class", "search");
+  inputField.setAttribute("placeholder", "What Lyrics do You Have in Mind?");
 
   if (/#access_token\=.+/.test(location.href)) {
     document.getElementById("connect-container").style.display = "none";
@@ -34,7 +33,7 @@ window.onload = function() {
 }
 
 function connectSpotify() {
-  window.open("http://accounts.spotify.com/authorize?client_id=1ef967355e974276ac8b3c14ef511e36&redirect_uri=https%3A%2F%2Fd-haran.github.io&scope=user-read-private%20user-read-email&response_type=token&show_dialog=true","_blank");
+  window.open("https://accounts.spotify.com/authorize?client_id=1ef967355e974276ac8b3c14ef511e36&redirect_uri=http%3A%2F%2F127.0.0.1:5500/LyricFind_ThetaHacks/SPEECH2Text.html&scope=user-read-private%20user-read-email&response_type=token&show_dialog=true","_blank");
 }
 
 function continueWithoutSpotify() {
@@ -42,63 +41,7 @@ function continueWithoutSpotify() {
   spotify = false;
 }
 
-function updateResults() {
-  if (accessToken !== "" || !spotify) {
-    resultsContainer.innerHTML = "";
-    var r = document.querySelector(".gsc-expansionArea").children[0];
 
-    var text = "";
-    var link = "";
-    var allGood = true;
-    try {
-      text = r.children[0].children[0].children[0].children[0].innerHTML.replace(/genius/ig, "").replace(/\(?lyrics\)?/ig, "").replace(/\.{3}/g, "").replace(/\|/g, "").replace(/<\/?b>/g,"").trim();
-      link = r.children[0].children[0].children[0].children[0].href;
-    } catch (e) {
-      console.log(e);
-      resultsContainer.innerHTML = "We couldn't find your song :(";
-      allGood = false;
-    }
-    
-    if (text.includes("Songs, and Albums")) {
-      allGood = false;
-      resultsContainer.innerHTML = "Your query seems to be a musician. Please search for lyrics only.";
-    }
 
-    if (allGood && spotify) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', `https://api.spotify.com/v1/search?q=${encodeURIComponent(text)}&type=track`, true);
-      xhr.setRequestHeader('Accept', 'application/json');
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-      xhr.send();
 
-      xhr.onload = function(e) {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          var response = JSON.parse(xhr.responseText);
-          try {
-            resultsContainer.innerHTML = getTrackHTML(response.tracks.items[0].uri);
-          } catch (e) {
-            console.log(e);
-            resultsContainer.innerHTML = `Your song seems to be <a target="_blank" href="${link}"><u><b>${text}</b></u></a>, but we couldn't find it on Spotify :(`;
-          }
-        } else {
-          alert("Your Spotify session has timed out.");
-        }
-      }
-    } else if (!spotify && allGood) {
-      resultsContainer.innerHTML = `<a target="_blank" href="${link}"><u><b>${text}</b></u></a>`;
-    }
-  }
-}
 
-function getTrackHTML(id) {
-  return `
-    <iframe 
-      src="https://open.spotify.com/embed/track/${id.replace('spotify:track:','')}" 
-      width="500" 
-      height="580"
-      frameborder="0"
-      allowtransparency="true"
-      allow="encrypted-media">
-    </iframe>`
-}
