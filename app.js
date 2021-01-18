364f402e7793400a4c069bbcc27f8d636b656d97
D-Haran <57646458+D-Haran@users.noreply.github.com>
2021-01-17 21:01:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,7 @@ var resultsContainer = document.querySelector("#results");
 var accessToken = "";
 var spotify = true;
 
-class SpeechRecognitionApi{
-    constructor(options) {
-    const SpeechToText = window.speechRecognition || window.webkitSpeechRecognition;
-    this.speechApi = new SpeechToText();
-    this.output = options.output ? options.output : document.createElement('div');
-    this.speechApi.continuous = true;
-    this.speechApi.internResult = false;
-    this.speechApi.onresult = (event) => {
-        var resultIndex = event.resultIndex;
-        var transcript = event.results[resultIndex][0].transcript;
-        this.output.textContent = transcript;
-        console.log(this.output)
-    }
 
-    }
-    init() {
-        this.speechApi.start();
-    }
-    stop(){
-        this.speechApi.stop();
-
-    }
-}
 window.onload = function () {
     var speech = new SpeechRecognitionApi({
 
