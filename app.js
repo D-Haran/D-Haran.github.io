75f9bf56f93cca4a9d7dd543a0e7d7100dbfc5a5
D-Haran <57646458+D-Haran@users.noreply.github.com>
2021-01-17 21:09:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ var accessToken = "";
 var spotify = true;
 
 
+class SpeechRecognitionApi{
+    constructor(options) {
+    const SpeechToText = window.speechRecognition || window.webkitSpeechRecognition;
+    this.speechApi = new SpeechToText();
+    this.output = options.output ? options.output : document.createElement('div');
+    this.speechApi.continuous = true;
+    this.speechApi.internResult = false;
+    this.speechApi.onresult = (event) => {
+        var resultIndex = event.resultIndex;
+        var transcript = event.results[resultIndex][0].transcript;
+        this.output.textContent = transcript;
+        console.log(this.output)
+    }
+
+    }
+    init() {
+        this.speechApi.start();
+    }
+    stop(){
+        this.speechApi.stop();
+
+    }
+}
 window.onload = function () {
     var speech = new SpeechRecognitionApi({
 
@@ -17,6 +40,7 @@ window.onload = function () {
         speech.stop();
     })
 }
+
 
 window.onload = function() {
   var inputField = document.getElementById("gsc-i-id1");
